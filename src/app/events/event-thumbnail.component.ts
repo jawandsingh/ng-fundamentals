0e03983159cacaeb807c901db10ef0d5a9801414
Jawand Singh <jawand.virk@gmail.com>
2018-06-12 00:12:58 +0530

    added interpolation and expressions on events page.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,20 +4,31 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
    selector: 'event-thumbnail',
    template: `
    <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngClass] = "getStartTimeClass()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
         </div>
         <!-- <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button> -->
     </div>
    `,
    styles:[`
     .pad-left{ margin-left: 10px;}
-    .well div { color: #bbb}
+    .well div { color: #bbb;}
+    .thumbnail {min-height: 210px;}
+    .green { color: #003300 !important;}
+    .bold { font-weight: bold;}
    `]
 })
 
@@ -37,4 +48,10 @@ export class EventThumbnailComponent{
     // logFoo(){
     //  console.log('foo');   
     // }
+
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold'];
+        return [''];
+    }
 }
