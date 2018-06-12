5579ff8479e491e407b1fbdfec4e25044ac7b32f
Jawand Singh <jawand.virk@gmail.com>
2018-06-13 00:15:30 +0530

    added two new services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from "./nav/navbar.component";
+import { EventService } from "./events/shared/event.service";
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
